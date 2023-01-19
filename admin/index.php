c34917524221a5045f0a9c577399610acde63c9f
lynlen <m.daffa342@gmail.com>
2023-01-19 09:36:39 +0700

    ganti title admin

--- admin/index.php
+++ admin/index.php
@@ -41,7 +41,7 @@ if (isset($_POST['delete'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>XI RPL DAOA</title>
+  <title>XI RPL DAOA - Admin</title>
   <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
 </head>
 
